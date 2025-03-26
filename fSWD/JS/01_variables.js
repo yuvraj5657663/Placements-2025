598d1343759107ba9c1d242b094a4e943a31f60e
yuvraj5657663 <yuvrajkumar4588@gmail.com>
2025-03-27 04:02:52 +0530

    js-fonudation

---
+++ fSWD/JS/01_variables.js
@@ -0,0 +1,21 @@
+const enrollmentId = 2202030400212
+let enrollmentEmail = "yuvrajkumar4588@gmail.com"   
+var enrollmentPassword = "yuvi123@123"
+enrollmentCity = "Ahmedabad"
+
+
+// enrollmentId = 2202030400210  // not allowed -> const keyboard are not change 
+
+
+enrollmentEmail = "yk811211kmp@gmail.com"
+enrollmentPassword = "xyz@1213"
+enrollmentCity = "Bihar"
+
+
+// console.log(enrollmentId);    -> single line comments
+
+/*  -> multiline comments start
+prefer not to use var
+because of  issue in block scope and funcitonal scope 
+*/  //-> multiline comments closed
+console.table([enrollmentId, enrollmentEmail, enrollmentCity, enrollmentPassword]);
---
+++ fSWD/JS/02_dataTypes.js
@@ -0,0 +1,27 @@
+//  "use strict"  -> treat all js code as newer version
+
+// alert(3+3)  -> we are using node js , not browser
+
+
+console.log(3 + 3);
+console.log("yuvraj")
+
+
+let name = "yuvraj"
+let age = 21
+let isLoggedIn = false 
+
+//  number => 2 to power 53
+//  bigInt =>
+//  String => ""
+//  boolean => true/false
+//  null  => standalone vlaue -> empty value /assig bhi kar sakte hai such tempreture '0'
+//  undefined =>
+//  symbol => unique
+
+// obejct
+
+console.log(typeof "yuvraj");
+console.log(typeof age);
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
